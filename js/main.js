5d68903840da9199d61e5ace73ce70a4fe8fc83e
dit001 <52833956+dit001@users.noreply.github.com>
2023-08-18 02:08:30 -0400

    bug

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ var gameData = {
 }
 
 function tut1advance() {
-	gameData.playerName.value = document.getElementById("playerName").value;
-	document.getElementById("tut2name").innerHTML = gameData.playerName.value;
+	gameData.playerName = document.getElementById("playerName").value;
+	document.getElementById("tut2name").innerHTML = gameData.playerName;
 	document.getElementById("tut1").hidden = true;
 	document.getElementById("tut2").hidden = false;
 }
